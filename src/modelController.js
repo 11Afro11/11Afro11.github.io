ac9ea13891fa036e04a8272dd231769c53509872
Fco. Javier Fuentes Barragán <javier.fuentes78@gmail.com>
2021-01-07 17:19:28 +0100

    clean

--- src/modelController.js
+++ src/modelController.js
@@ -22,50 +22,11 @@
         requestIdleCallback(update);
       }
   
-      if (upKeyPressed) {
-        if (speedZ < maxSpeed) {
-          speedZ += accel;
-        }
-      } else if (downKeyPressed) {
-        if (speedZ > -maxSpeed) {
-          speedZ -= accel;
-        }
-      } else {
-        if (speedZ > accel) {
-          speedZ -= accel;
-        } else if (speedZ < -accel) {
-          speedZ += accel;
-        } else {
-          speedZ = 0;
-        }
-      }
-  
-      if (leftKeyPressed) {
-        if (speedX < maxSpeed) {
-          speedX += accel;
-        }
-      } else if (rightKeyPressed) {
-        if (speedX > -maxSpeed) {
-          speedX -= accel;
-        }
-      } else {
-        if (speedX > accel) {
-          speedX -= accel;
-        } else if (speedX < -accel) {
-          speedX += accel;
-        } else {
-          speedX = 0;
-        }
-      }
+     
   
       cameraRotationX = Math.max(cameraRotationX, -Math.PI / 2);
       cameraRotationX = Math.min(cameraRotationX, Math.PI / 2);
-  
-      cameraPositionX -= Math.cos(cameraRotationY) * speedX;
-      cameraPositionZ -= Math.sin(cameraRotationY) * speedX;
-  
-      cameraPositionX += Math.sin(cameraRotationY) * speedZ;
-      cameraPositionZ -= Math.cos(cameraRotationY) * speedZ;
+
   
       scene.style.transform =
         "translateZ(701px)" +
@@ -76,22 +37,9 @@
         cameraRotationY.toFixed(6) +
         "rad)";
   
-      room.style.transform =
-        "translate3d(" + -cameraPositionX + "px,0," + -cameraPositionZ + "px)";
     };
   
-    const keyHandler = e => {
-      let keyPressed = e.type === "keydown";
-      if (e.code === "KeyW") {
-        upKeyPressed = keyPressed;
-      } else if (e.code === "KeyS") {
-        downKeyPressed = keyPressed;
-      } else if (e.code === "KeyA") {
-        leftKeyPressed = keyPressed;
-      } else if (e.code === "KeyD") {
-        rightKeyPressed = keyPressed;
-      }
-    };
+
   
     const mouseHandler = e => {
       cameraRotationY += e.movementX / 150;
@@ -99,22 +47,15 @@
     };
   
     const start = () => {
-      document.addEventListener("keyup", keyHandler);
-      document.addEventListener("keydown", keyHandler);
       document.addEventListener("mousemove", mouseHandler);
       paused = false;
       update();
     };
   
     const stop = () => {
-      document.removeEventListener("keyup", keyHandler);
-      document.removeEventListener("keydown", keyHandler);
       document.removeEventListener("mousemove", mouseHandler);
       paused = true;
-      upKeyPressed = false;
-      downKeyPressed = false;
-      leftKeyPressed = false;
-      rightKeyPressed = false;
+
     };
   
     document.addEventListener("click", () => {
